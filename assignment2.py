92e3376da9b9eec48e6a3abbd809f62aee8c83a8
AnsariAffan <90125265+AnsariAffan@users.noreply.github.com>
2021-11-08 22:43:32 +0530

    assignment2
    area of circle
    pelindrome number

---
+++ assignment2.py
@@ -0,0 +1,18 @@
+
+#formula of area of circle = 2*3.14*radius
+radius =  int(input("enter the radius of a circle "))
+area_of_circle = 2*3.14*radius
+print(area_of_circle)
+
+
+#palindrome number
+input_number = int(input("Enter a number "))
+number = str(input_number)
+
+if(number==number[::-1]):
+    print("palindrome number")
+else:
+    print("not a palindrome number")
+    
+
+
